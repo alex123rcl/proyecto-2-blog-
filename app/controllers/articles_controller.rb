ba5d663f71e85d5ef5c793ae1f340b8c79414f1c
ronald caballero <caballerolinarter123@gmail.com>
2024-11-11 17:01:51 -0600

    actializacion

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,55 +1,56 @@
 class ArticlesController < ApplicationController
-
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def new
     @article = Article.new
   end
 
   def create
-    @article = current_user.articles.build(article_params)
+    @article = current_user.articles.build(article_params)  # Correctamente instanciamos el artículo asociado al usuario
     if @article.save
       redirect_to @article
     else
       render 'new'
     end
   end
-  
 
   def show
-    @article = Article.find(params[:id])
+    # No es necesario definir @article aquí porque ya se setea en el before_action
     @comments = @article.comments.includes(:user)
   end
-  
   
   def index
     @articles = Article.all
   end
 
   def edit
-    @article = Article.find(params[:id])
+    # No es necesario definir @article aquí porque ya se setea en el before_action
   end 
 
   def update
-    @article = Article.find(params[:id])  
-    if @article.update(article_params)    
-      redirect_to @article                
+    if @article.update(article_params)
+      redirect_to @article
     else
-      render 'edit'                       
+      render 'edit'
     end
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    if @article.user == current_user
+    if @article.user == current_user  # Solo el propietario del artículo puede destruirlo
       @article.destroy
-      redirect_to articles_path, notice: 'borrado'
+      redirect_to articles_path, notice: 'Artículo borrado'
     else
-      redirect_to articles_path, alert: 'sin permisos .'
+      redirect_to articles_path, alert: 'No tienes permiso para realizar esta acción.'
     end
   end
-  
 
   private
+
+  # Asegúrate de que @article se setee correctamente para las acciones correspondientes
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :text)
